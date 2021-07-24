a1dfbc9323ddabaea9c70a471906c2f4d7c679ca
Julian Cano VR <juliancano.dev@gmail.com>
2021-07-24 18:34:55 -0500

    Navbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from '@angular/material/toolbar';
+//import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   exports: [MatToolbarModule]
